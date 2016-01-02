18217c3e7a848fcad85c61b97ff5bb7316e182b1
anders617 <anders617@gmail.com>
2016-01-01 22:00:33 -0500

    Initial Commit

---
+++ ABMatrices/ABMatrices.h
@@ -0,0 +1,19 @@
+//
+//  ABMatrices.h
+//  ABMatrices
+//
+//  Created by Anders Boberg on 1/1/16.
+//  Copyright © 2016 Anders boberg. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ABMatrices.
+FOUNDATION_EXPORT double ABMatricesVersionNumber;
+
+//! Project version string for ABMatrices.
+FOUNDATION_EXPORT const unsigned char ABMatricesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ABMatrices/PublicHeader.h>
+
+
